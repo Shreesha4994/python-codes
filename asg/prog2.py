1f708a42fa2fb40f2d2fe54c09cc16deb3edb308
Shreesha4994 <44250321+Shreesha4994@users.noreply.github.com>
2021-05-07 13:39:35 +0530

    Changed logic
    Small change in 400 leap year logic

--- asg/prog2.py
+++ asg/prog2.py
@@ -2,9 +2,9 @@
 year=int(input("Enter year"))
 if(year%4==0 and year%100!=0):
     res='Leap'
-elif(year%100==0):
-    res='Not Leap'
 elif(year%400==0):
+    res='Not Leap'
+elif(year%100==0):
     res='Leap'
 else:
     res='Not Leap'
