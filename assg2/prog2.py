18b55ee018a956cc621d988ebe5929685aea8eea
Shreesha4994 <44250321+Shreesha4994@users.noreply.github.com>
2021-06-12 18:40:15 +0530

    CHANGED FILE NAMES VOWEL AND FROM

--- assg2/prog2.py
+++ assg2/prog2.py
@@ -1,7 +1,7 @@
 #copy each line starting with vowel to another file
 vowels=['A','a','E','e','i','o','u','I','O','U']
-file = open("sample3.txt","r")
-f = open("vowels.txt","a")
+file = open("FROM.txt","r")
+f = open("VOWELS.txt","a")
 for line in file:
 	if(line[0] in vowels):
 		f.write(line)
